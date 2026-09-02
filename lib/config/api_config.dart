653d8c390fd6b894388789a9f532e87efb447e22
Balaji <balaji.focuslogic@gmail.com>
2026-09-02 16:21:35 +0530

    Api Config

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -3,10 +3,10 @@ import 'package:flutter/foundation.dart';
 class ApiConfig {
   static String get baseUrl {
     if (kIsWeb) {
-      // Using local IP to allow both PC and Mobile (same network) to connect
-      return 'http://192.168.1.47:5260/api';
+      // Flutter Web - localhost-la run panna localhost use pannanum
+      return 'http://localhost:5260/api';
     }
-    // For Android/iOS emulator or mobile app
+    // For Android/iOS mobile app - LAN IP use pannanum
     return 'http://192.168.1.47:5260/api';
   }
 }
