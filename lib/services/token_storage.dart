ffc2c3c0274988671fbca4d78c9a091d65c078ea
Balaji <balaji.focuslogic@gmail.com>
2026-09-11 11:30:26 +0530

    feat: add cross-platform TokenStorage service using secure storage for mobile and shared preferences for web

--- lib/services/token_storage.dart
+++ lib/services/token_storage.dart
@@ -1,18 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenStorage {
   static const _storage = FlutterSecureStorage();
   static const _keyToken = 'jwt_token';
 
   static Future<void> saveToken(String token) async {
-    await _storage.write(key: _keyToken, value: token);
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_keyToken, token);
+    } else {
+      await _storage.write(key: _keyToken, value: token);
+    }
   }
 
   static Future<String?> getToken() async {
-    return await _storage.read(key: _keyToken);
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      return prefs.getString(_keyToken);
+    } else {
+      try {
+        return await _storage.read(key: _keyToken);
+      } catch (_) {
+        final prefs = await SharedPreferences.getInstance();
+        return prefs.getString(_keyToken);
+      }
+    }
   }
 
   static Future<void> deleteToken() async {
-    await _storage.delete(key: _keyToken);
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove(_keyToken);
+    } else {
+      try {
+        await _storage.delete(key: _keyToken);
+      } catch (_) {}
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove(_keyToken);
+    }
   }
 }
